8f462727cfa9538d5cc3b4f85dc3628ca938b95c
Pawel Mikolajewski <mikolajewskip@gmail.com>
2010-06-14 21:03:52 +0200

    Version bump, emotions, more dialogs, substitution me and you

--- lib/johndoe/responser.rb
+++ lib/johndoe/responser.rb
@@ -1,5 +1,12 @@
 
 module JohnDoe
+  class Response
+    attr_accessor :text, :emotion
+    def initialize(t,e)
+      @text = t
+      @emotion = e[rand(e.size)]
+    end
+  end
   class Responser
     def initialize(data)
       @data = data
@@ -8,12 +15,10 @@ module JohnDoe
     def response(sentence)
       @data.patterns.each do |k,v|
         if (/^#{k}/i =~ sentence)
-          
-          return sub_v random_quote(@data.responses[v], /^#{k}/i.match(sentence).captures)
+          return Response.new(sub_v(random_quote(@data.responses[v[:resp]], /^#{k}/i.match(sentence).captures)),v[:emotions])
         end
       end
-
-      return sub_v random_quote(@data.default["dontunderstand"])
+      return Response.new(sub_v(random_quote(@data.default["dontunderstand"])),["none"])
     end
 
     #substitute variables
@@ -37,7 +42,11 @@ module JohnDoe
 
     def subject_replace(s,subjects = [])
       return s if subjects.empty?
-      return s.gsub("$",subjects[0])
+      return s.gsub("$",describe_who(subjects[0]))
+    end
+
+    def describe_who(s)
+      s.gsub(/([^a-z])me([^a-z]|$)/,"\\1###\\2").gsub(/([^a-z])you([^a-z]|$)/,"\\1me\\2").gsub("###","you")
     end
   end
 end
