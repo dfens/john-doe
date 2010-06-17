20bba1fa5ef04e96508baab4c119d501b66c101a
Pawel Mikolajewski <mikolajewskip@gmail.com>
2010-06-18 00:07:13 +0200

    Minor fix

--- lib/john-doe.rb
+++ lib/john-doe.rb
@@ -5,7 +5,11 @@ require File.dirname(__FILE__)+'/johndoe/markov'
 
 module JohnDoe
   class ChatBot
-    def initialize(filename = File.dirname(__FILE__) + "/../default.yml" , quotes_filename = File.dirname(__FILE__) + "/../quotes")
+    def initialize(filename = File.dirname(__FILE__) + "/../default.yml" , quotes_filename = "quotes")
+      unless File.exists? quotes_filename
+        f = File.new(quotes_filename,"w")
+        f.close
+      end
       @loader = JohnDoe::Aiml.new
       @loader.load(filename)
       @responser = JohnDoe::Responser.new(@loader,quotes_filename)
